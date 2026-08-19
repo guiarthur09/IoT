3ed8c6c091c23addbd1e64be93bf994d08fad157
Guilherme <guilhermearthursilveira13@gmail.com>
2026-08-19 20:02:04 -0300

    fix: arrumando logica do fluxo

--- num-ordenados.c
+++ num-ordenados.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 
-int main (){
-	
-	int n1, n2;
-	
-	printf("Digite o Numero 1: ");
-	scanf("%d", &n1);
-	
-	printf("Digite o Numero 2: ");
-	scanf("%d", &n2);
-	
-	if (n1 == n2){
-		printf("Error! Dois Numeros iguais");
-	}
-	
-	else if (n2 == n1){
-		printf("Error! Dois Numeros iguais");
-	} 
-	
-	else if (n1>n2){
-		printf("Numero 1: %d\nNumero 2: %d\nOrdenado: %d - %d", n1, n2, n1, n2);
-	}
-	 
-	else if (n2>n1){
-		printf("Numero 1: %d\n Numero 2: %d\n Ordenado: %d - s%d", n2, n1, n2, n1);
-}	}	
+int main () {
+    int n1, n2;
+    
+    printf("Digite o Numero 1: ");
+    scanf("%d", &n1);
+    
+    printf("Digite o Numero 2: ");
+    scanf("%d", &n2);
+    
+    if (n1 == n2) {
+        printf("Error! Dois Numeros iguais\n");
+    } 
+    else if (n1 > n2) {
+        printf("Numero 1: %d\nNumero 2: %d\n", n1, n2);
+        printf("Ordenado: %d - %d\n", n1, n2);
+    } 
+    else {
+        printf("Numero 1: %d\nNumero 2: %d\n", n1, n2);
+        printf("Ordenado: %d - %d\n", n2, n1);
+    }
+    
+    return 0;
+}
